59a70fe9aebf4d2d68a6bca3419bac070e2b3a50
Franco <frfantusatti@protonmail.com>
2026-02-14 01:42:53 -0300

    [8 Kyu] Complete the method that takes a boolean value

---
+++ convert-boolean-values-to-strings.py
@@ -0,0 +1,9 @@
+def bool_to_word(boolean):
+    if boolean == True:
+        return "Yes"
+    else:
+        return "No"
+        
+bool_to_word(False)
+
+# La función bool_to_word toma un valor booleano como argumento. Si el valor es True, devuelve la cadena "Yes". Si el valor es False, devuelve la cadena "No". En este caso, al llamar a bool_to_word(False), la función devolverá "No".
