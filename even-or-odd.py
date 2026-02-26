fd7d183ac0fe854030c2532a411f5f31e484f3c7
Franco <frfantusatti@protonmail.com>
2026-02-26 03:10:06 -0300

    [8 Kyu] Create a function that takes an integer as an argument and returns Even for even numbers or Odd for odd numbers.

---
+++ even-or-odd.py
@@ -0,0 +1,11 @@
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+even_or_odd(4)
+even_or_odd(5)
+
+# La funcion even_or_odd toma un numero como parametro y devuelve "Even" si el numero es par y "Odd" si el numero es impar.
