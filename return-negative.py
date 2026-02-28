64b1dbe9ff8e2997a1f8ac9f91ba197142632110
Franco <frfantusatti@protonmail.com>
2026-02-28 03:23:26 -0300

    [8 Kyu] In this simple assignment you are given a number and have to make it negative.

---
+++ return-negative.py
@@ -0,0 +1,12 @@
+def make_negative(number):
+    return -abs(number)
+
+
+make_negative(1)
+make_negative(2)
+make_negative(-3)
+
+# Esta funcion convierte un numero en negativo si es positivo.
+# Si el numero es negativo, devuelve el numero sin cambios.
+# Si el numero es cero, devuelve cero.
+# Devuelve el numero convertido en negativo.
