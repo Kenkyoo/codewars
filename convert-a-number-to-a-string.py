09658d5ccefdb22ae7d648a9873d6bf882926764
Franco <frfantusatti@protonmail.com>
2026-03-05 06:51:58 -0300

    [8 Kyu] We need a function that can transform a number (integer) into a string.

---
+++ convert-a-number-to-a-string.py
@@ -0,0 +1,10 @@
+def number_to_string(num):
+    return str(num)
+
+number_to_string(123)
+number_to_string(-100)
+
+#Explicacion del codigo
+# La función `number_to_string` toma un número como argumento y devuelve su representación en forma de cadena.
+# La función utiliza la función incorporada `str()` para convertir el número a una cadena.
+# Luego, se llaman a la función `number_to_string` con dos ejemplos: el número
